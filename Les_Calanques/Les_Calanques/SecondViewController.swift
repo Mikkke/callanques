fda60951f8863c0db1a34a7020051b060bc70bb4
Joseph Michael <jkellymichael@gmail.com>
2019-07-24 16:52:44 +0200

    the rest

--- Les_Calanques/Les_Calanques/SecondViewController.swift
+++ Les_Calanques/Les_Calanques/SecondViewController.swift
@@ -12,6 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("salut")
         // Do any additional setup after loading the view.
     }
 
